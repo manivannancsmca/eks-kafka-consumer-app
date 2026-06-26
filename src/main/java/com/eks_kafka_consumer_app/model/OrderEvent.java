0d856fd557e61241c1600e476ffe69d71f07949a
manivannancsmca <manivannancsmca@gmail.com>
2026-06-26 21:30:52 +0530

    Add body to OrderEvent record
    Added a body to the OrderEvent record for potential future methods.

--- src/main/java/com/eks_kafka_consumer_app/model/OrderEvent.java
+++ src/main/java/com/eks_kafka_consumer_app/model/OrderEvent.java
@@ -1,3 +1,5 @@
 package com.eks_kafka_consumer_app.model;
 
-public record OrderEvent(String orderId, String item, double price, String status) {}
+public record OrderEvent(String orderId, String item, double price, String status) {
+  
+}
